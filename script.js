550c2081b547ac22c29e468fa5717fd972a71988
fk00750 <fk_00750@yahoo.com>
2022-04-07 11:18:46 +0530

    version 2

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function auto() {
   } else {
     num++;
   }
-  img.src = `images/img_${num}.jpg`;
+  img.src = `images/img_${num}_mobile.jpg`;
 }
 
-setInterval(auto, 4000);
+// setInterval(auto, 4000);
 
 //////////////////////////////////////////
 
@@ -105,8 +105,9 @@ const body = document.querySelector("body");
 
 // console.log(body.firstElementChild);
 // console.log();
-body.lastElementChild.previousSibling.previousSibling.style.backgroundColor =
-  "black";
+// ? footer
+// body.lastElementChild.previousSibling.previousSibling.style.backgroundColor =
+//   "black";
 
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
@@ -125,30 +126,73 @@ body.lastElementChild.previousSibling.previousSibling.style.backgroundColor =
 
 // console.log();
 
+// ! ReadMore and ReadLess
+let ticketContainer;
+
+const Read_less_OR_more = function (target, txt, hexCode) {
+  target.previousElementSibling.lastElementChild.classList.toggle("hidden");
+  target.textContent = `${txt}`;
+  target.style.backgroundColor = `#${hexCode}`;
+};
+
 // const Destinations = document.getElementById('destinations');
 document.querySelector("#destinations").addEventListener("click", function (e) {
   // console.log(e.target.closest('#content').childNodes[3].children);
   //////////////////////////////
 
-  if (e.target.id === "ReadMorebtn") {
-    e.target.previousElementSibling.lastElementChild.classList.toggle("hidden");
+  if (
+    e.target.id === "ReadMorebtn" &&
+    e.target.previousElementSibling.lastElementChild.classList.contains(
+      "hidden"
+    )
+  ) {
+    Read_less_OR_more(e.target, "Read Less", "9B9BCC");
+  } else if (
+    e.target.id === "ReadMorebtn" &&
+    !e.target.previousElementSibling.lastElementChild.classList.contains(
+      "hidden"
+    )
+  ) {
+    Read_less_OR_more(e.target, "Read More", "DC2626");
   }
+
+  /////////////////////////////////////////////////////////
+  ticketContainer = document.querySelector(".ticket_modal");
+  let cityName = document.querySelector(".cityName");
+  if (e.target.id === "BuyTicket") {
+    ticketContainer.classList.remove("hidden");
+    cityName.textContent =
+      e.target.closest("div").firstElementChild.textContent;
+  }
+});
+
+const btnCloseModal = document.querySelector(".btn--close-modal");
+btnCloseModal.addEventListener("click", function () {
+  ticketContainer.classList.add("hidden");
 });
 
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
 
 const OperationsContainer = document.querySelector("#Operations_section");
-const tabs = document.querySelectorAll("operations__tab");
+const tabs = document.querySelectorAll(".operations__tab");
 const tabsContent = document.querySelectorAll(".operation_content");
 OperationsContainer.addEventListener("click", function (e) {
   ////////////////////////////////
   // console.log(e.target);
   const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
   // console.log(clicked);
 
   tabsContent.forEach((content) => {
     content.classList.add("hidden");
+  });
+
+  tabs.forEach((tab) => {
+    tab.classList.add("opacity-60");
+    tab.classList.remove("border");
+    e.target.classList.remove("opacity-60");
+    e.target.classList.add("border");
   });
 
   document
@@ -168,44 +212,132 @@ OperationsContainer.addEventListener("click", function (e) {
 // console.log(header.classList);
 
 // const stickyNav = function (entries) {
-  //   // console.log(entries);
-  //   const [entry] = entries
-  //   console.log(entry);
-  //   // if (!entry.isIntersecting) {
-    //   //   header.classList.add('fixed')
-    //   // } else {
-      //   //   // header.classList.remove('fixed')
-      
-      //   // }
-      // }
-      
-      // const headerObserver = new IntersectionObserver(stickyNav , {
-        //   root: null,
-        //   threshold:0.10,
+//   // console.log(entries);
+//   const [entry] = entries
+//   console.log(entry);
+//   // if (!entry.isIntersecting) {
+//   //   header.classList.add('fixed')
+//   // } else {
+//   //   // header.classList.remove('fixed')
+
+//   // }
+// }
+
+// const headerObserver = new IntersectionObserver(stickyNav , {
+//   root: null,
+//   threshold:0.10,
 // })
 
 // headerObserver.observe(header)
 
-const nav = document.querySelector('.nav')
-const header = document.getElementById('header')
+const nav = document.querySelector(".nav");
+const header = document.getElementById("header");
 
 const stickyNav = function (entries) {
   //////////////////
   // entry details h
   const [entry] = entries;
   // ? !entry.isIntersecting -> agar header interact nahi krega viewport se according to threshold
-  if(!entry.isIntersecting) {
-     nav.classList.add('fixed')
+  if (!entry.isIntersecting) {
+    nav.classList.add("fixed");
   } else {
-    nav.classList.remove('fixed')
+    nav.classList.remove("fixed");
   }
-}; 
+};
 
 const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null, // ? viewport 
+  root: null, // ? viewport
   threshold: 0.5, // ? visiblity in viewport i.e 50%
 });
 
+// headerObserver.observe(header)
 
+// console.log(document.querySelector('.modal'))
 
-headerObserver.observe(header)
+///////////////////////////////////////////////////////////
+
+const Confirmbtn = document.getElementById("ConfirmTicket");
+const usr_ticket__details = document.querySelectorAll(".user_ticketDetail");
+const usrName = document.querySelector("#user_ticket_name");
+const usrEmail = document.querySelector("#user_ticket_email");
+const usrTele = document.querySelector("#user_ticket_tele");
+const usrLocation = document.querySelector("#user_ticket_location");
+const usrDate = document.querySelector("#user_ticket_date");
+const TicketModal = document.querySelector(".ticket_modal");
+
+usr_ticket__details.forEach(function (elem, i) {});
+
+Confirmbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  TicketModal.innerHTML = "";
+  const html = `<button class="btn--close-modal absolute -top-0.5 text-4xl right-2">&times;</button>
+  <h2 class="model__header mt-6 text-2xl">Congratulations Your Ticket Has Been Booked !!!</h2>
+   <span class="text-2xl">To <i>winderberg</i></span> 
+  <div class="modal__form flex flex-col space-y-2 text-xl font-mono py-2" >
+    <table class="border-2 my-4 text-left">
+      <tr>
+        <th  class="px-2 py-2">From</th>
+        <td>${usrLocation.value}</td>
+      </tr>
+      <tr>
+        <th class="px-2 py-2">To</th>
+        <td>Winderberg</td>
+      </tr>
+      <tr>
+        <th class="px-2 py-2">Name</th>
+        <td>${usrName.value}</td>
+      </tr>
+      <tr>
+        <th class="px-2 py-2">Flight</th>
+        <td>SUUV-778C52</td>
+      </tr>
+      <tr>
+        <th class="px-2 py-2">Date</th>
+        <td>${usrDate.value}</td>
+      </tr>
+      <tr>
+        <th class="px-2 py-2">Timing</th>
+        <td>hello</td>
+      </tr>
+    </table>
+    <button type="submit" class="bg-gray-900 text-white w-fit absolute bottom-1 right-2 text-base m-2">Download</button> 
+  </div>`;
+  TicketModal.insertAdjacentHTML("afterbegin", html);
+});
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+
+// ! lazy Loading Images
+
+const ImgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // ? it is lazy img at first
+  // console.log(entry.target.src); // img_1_lazy.jpg
+  entry.target.src = entry.target.dataset.src; // ? now Here it becme --- img_1_mobile.jpg
+  // console.log(entry.target.src); // img_1_mobile.jpg
+
+  // * When the img_1_mobile gets fullyLoaded then display it
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("blur-sm");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  ////
+  root: null,
+  threshold: 0,
+});
+
+ImgTargets.forEach((img) => {
+  imgObserver.observe(img);
+});
